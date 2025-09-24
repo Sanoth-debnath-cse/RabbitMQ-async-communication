647e94225334a69234a46b7a636e8f271f6e16e5
Sanoth-debnath-cse <sanoth14@cse.pstu.ac.bd>
2025-09-24 15:06:14 +0600

    Implemented Publisher

---
+++ rabbitmq-string-demo/src/main/java/com/rabbitmqguide/rabbitmqstringdemo/publisher/RabbitMQProducer.java
@@ -0,0 +1,31 @@
+package com.rabbitmqguide.rabbitmqstringdemo.publisher;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQProducer {
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    private static final Logger logger = LoggerFactory.getLogger(RabbitMQProducer.class);
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public RabbitMQProducer(RabbitTemplate rabbitTemplate){
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String message){
+        logger.info("Sending message: {}", message);
+        rabbitTemplate.convertAndSend(exchangeName, routingKey, message);
+    }
+}
